038ffb37ac92bccf69c1c970124e7962f6d2079d
Sri Padma Chintha <sripadmachintha@gmail.com>
2025-06-21 13:03:36 +0530

    create product route and its function

--- api/index.js
+++ api/index.js
@@ -6,25 +6,32 @@ const products = require("./data/Products");
 dotenv.config()
 const PORT = process.env.PORT;
 
+//connect to MongoDB
 const mongoose = require("mongoose")
 mongoose.connect(process.env.MONGOSEDB_URL).then(() => console.log("db connected")).then((err)=>{
     err;
 })
+
+//middleware
 app.use(express.json())
 
 // database seeder routes 
 const databaseSeeder = require('./databaseSeeder')
 const userRoute = require("./routes/User");
-const productRoute = require("../api/routes/Product")
+const productRoute = require("../api/routes/Product");
+const orderRoute = require("./routes/order");
 
 
 app.use('/api/seed',databaseSeeder)
 
 // routes for Users
-app.use('/api/users', userRoute)
+app.use('/api/users', userRoute);
 
 // routes for Products
-app.use('/api/products', productRoute)
+app.use('/api/products', productRoute);
+
+// routes for order
+app.use('/api/orders', orderRoute);
 
 app.listen(PORT || 5000,() => { 
     console.log(`server listening on port ${PORT}`);
--- api/models/Order.js
+++ api/models/Order.js
@@ -1,69 +1,51 @@
+const mongoose = require('mongoose');
 
-const mongoose = require("mongoose");
-
-const orderItemSchema =  mongoose.Schema({
-  name: { type: String, required: true },
-  qty: { type: Number, required: true },
-  image: { type: String, required: true },
-  price: { type: Number, required: true },
-  product: {
+const orderSchema = new mongoose.Schema({
+  user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Product",
     required: true,
+    ref: 'User'
   },
+  orderItems: [
+    {
+      name: { type: String, required: true },
+      qty: { type: Number, required: true },
+      price: { type: Number, required: true },
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Product'
+      }
+    }
+  ],
+  shippingAddress: {
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    country: { type: String, required: true }
+  },
+  paymentMethods: {
+    type: String,
+    required: true
+  },
+  shippingPrice: {
+    type: Number,
+    required: true
+  },
+  taxPrice: {
+    type: Number,
+    required: true
+  },
+  totalPrice: {
+    type: Number,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+}, {
+  timestamps: true
 });
 
-const orderSchema = new mongoose.Schema(
-  {
-    user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-    orderItems: [orderItemSchema],
-    shippingAddress: {
-      address: { type: String, required: true },
-      city: { type: String, required: true },
-      postalCode: { type: String, required: true },
-      country: { type: String, required: true },
-    },
-    paymentMethod: { type: String, required: true, default: "Paypal" },
-    paymentResult: {
-      id: { type: String },
-      status: { type: String },
-      updated_time: { type: String },
-      email_address: { type: String },
-    },
-    taxPrice: {
-      type: Number,
-      required: true,
-      default: 0.0,
-    },
-    shippingPrice: {
-      type: Number,
-      required: true,
-      default: 0.0,
-    },
-    totalPrice: {
-      type: Number,
-      required: true,
-      default: 0.0,
-    },
-    isPaid: {
-      type: Boolean,
-      required: true,
-      default: false,
-    },
-    paidAt: {
-      type: Date,
-    },
-    isDelivered: {
-      type: Boolean,
-      required: true,
-      default: false,
-    },
-    deliveredAt: {
-      type: Date,
-    },
-  },
-  { timestamps: true }
-);
-
-
-module.exports = mongoose.model("Order", orderSchema)
+module.exports = mongoose.model('Order', orderSchema);
